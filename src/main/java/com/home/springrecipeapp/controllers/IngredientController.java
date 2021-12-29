3967bad4a156573699ef142484d2e891b91538b0
Stanislavs Ostrovskis <st.ostrovskis@gmail.com>
2021-12-29 19:30:11 +0200

    small fixes

--- src/main/java/com/home/springrecipeapp/controllers/IngredientController.java
+++ src/main/java/com/home/springrecipeapp/controllers/IngredientController.java
@@ -9,7 +9,10 @@ import com.home.springrecipeapp.services.UnitOfMeasureService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Slf4j
 @Controller
@@ -79,8 +82,8 @@ public class IngredientController {
         return "recipe/ingredient/ingredientform";
     }
 
-    @GetMapping
-    @RequestMapping("recipe/{recipeId}/ingredient/{id}/delete")
+
+    @GetMapping("recipe/{recipeId}/ingredient/{id}/delete")
     public String deleteIngredient(@PathVariable String recipeId,
                                    @PathVariable String id){
 
